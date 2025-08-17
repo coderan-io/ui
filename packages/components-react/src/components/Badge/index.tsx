72c3975fd546a02eeb23b49cacfa77323e876717
eran <dev@eranmachiels.nl>
2025-08-17 22:54:08 +0200

    Fixed missing imports

--- packages/components-react/src/components/Badge/index.tsx
+++ packages/components-react/src/components/Badge/index.tsx
@@ -1,22 +1,2 @@
-import { FC, HTMLAttributes, PropsWithChildren } from 'react';
-import { Color } from '../../utilities/Color';
-import { clsx } from 'clsx';
-import styles from './badge.module.css';
-
-export interface BadgeProps extends HTMLAttributes<HTMLDivElement> {
-    color?: Color;
-}
-
-export const Badge: FC<PropsWithChildren<BadgeProps>> = ({
-    className,
-    color,
-    children,
-}) => (
-    <div className={clsx(
-        styles.cuiBadge,
-        styles[`cuiBadge--${color}`],
-        className,
-    )}>
-        {children}
-    </div>
-)
+export * from './Badge';
+export * from './BadgeColor';
