cbba7030f65fce3bf4b1fcd22162d5dbdeb61c14
EranNL <dev@eranmachiels.nl>
2025-12-29 21:19:12 +0100

    Added basic buttons

---
+++ packages/cui-react/src/components/Button/Button.tsx
@@ -0,0 +1,35 @@
+import type { FC, PropsWithChildren, ReactNode } from 'react';
+import { clsx } from 'clsx';
+import '@coderan/cui-styles/src/components/buttons.scss';
+
+export type ButtonProps = {
+    variant: 'primary';
+    loading: boolean;
+    iconStart?: ReactNode;
+    iconEnd?: ReactNode;
+}
+
+export const Button: FC<PropsWithChildren<ButtonProps>> = ({
+    children,
+    loading,
+    variant,
+    iconStart,
+    iconEnd,
+    ...props
+}) => {
+
+    return (
+        <button
+            className={clsx(
+                'cui-button',
+                `cui-button--${variant}`,
+                loading && 'cui-button--loading',
+            )}
+            {...props}
+        >
+            {iconStart && <i className="cui-button__icon cui-button__icon--start">{iconStart}</i>}
+            {children}
+            {iconEnd && <i className="cui-button__icon cui-button__icon--end">{iconEnd}</i>}
+        </button>
+    )
+}
---
+++ packages/playground/stories/Button.stories.ts
@@ -0,0 +1,44 @@
+import { Button, ButtonVariant } from '@coderan/cui-react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+    title: 'Example/Button',
+    component: Button,
+    args: {
+        children: 'Button'
+    },
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info:
+        // https://storybook.js.org/docs/configure/story-layout
+        layout: 'centered'
+    },
+    // This component will have an automatically generated Autodocs entry:
+    // https://storybook.js.org/docs/writing-docs/autodocs
+    // tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/api/argtypes
+    argTypes: {
+        loading: { control: 'boolean' },
+        variant: {
+            type: 'select',
+            options: Object.values(ButtonVariant)
+        },
+        iconStart: {
+            type: 'select',
+            // font awesome icons as options
+            options: ['👠', '👡', '👢', '👣', '👤', '👥', '👦', '👧', '👨', '👩', '👪', '👫', '👬', '👭', '👮', '👯', '👰', '👱', '👲', '👳', '👴', '👵', '👶', '👷', '👸', '👹', '👺', '👻', '👼', '👽', '👾', '👿', '💀', '💁', '💂', '💃', '💄', '💅', '💆', '💇', '💈', '💉', '💊', '💋', '💌', '💍', '💎', '💏', '💐', '💑', '💒', '💓', '💔', '💕', '💖', '💗', '💘', '💙', '💚', '💛', '💜', '💝', '💞', '💟', '💠', '💡', '💢', '💣', '💤', '💥', '💦', '💧', '💨', '💩', '💪', '💫', '💬', '💭', '💮', '💯', '💰', '💱', '💲', '💳', '💴', '💵', '💶', '💷', '💸', '💹', '💺', '💻', '💼', '💽', '💾', '💿', '📀', '📁', '📂', '📃', '📄', '📅', '📆', '📇', '📈', '📉', '📊',]
+        },
+        iconEnd: {
+            type: 'select',
+            // font awesome icons as options
+            options: ['👠', '👡', '👢', '👣', '👤', '👥', '👦', '👧', '👨', '👩', '👪', '👫', '👬', '👭', '👮', '👯', '👰', '👱', '👲', '👳', '👴', '👵', '👶', '👷', '👸', '👹', '👺', '👻', '👼', '👽', '👾', '👿', '💀', '💁', '💂', '💃', '💄', '💅', '💆', '💇', '💈', '💉', '💊', '💋', '💌', '💍', '💎', '💏', '💐', '💑', '💒', '💓', '💔', '💕', '💖', '💗', '💘', '💙', '💚', '💛', '💜', '💝', '💞', '💟', '💠', '💡', '💢', '💣', '💤', '💥', '💦', '💧', '💨', '💩', '💪', '💫', '💬', '💭', '💮', '💯', '💰', '💱', '💲', '💳', '💴', '💵', '💶', '💷', '💸', '💹', '💺', '💻', '💼', '💽', '💾', '💿', '📀', '📁', '📂', '📃', '📄', '📅', '📆', '📇', '📈', '📉', '📊',]
+        }
+    }
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary = {
+    args: {
+        variant: ButtonVariant.PRIMARY,
+        children: 'Primary Button'
+    }
+};
