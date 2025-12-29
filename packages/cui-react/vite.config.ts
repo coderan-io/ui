4b49af6b617a3a340da231ad99eae84857c0d624
EranNL <dev@eranmachiels.nl>
2025-12-29 21:20:21 +0100

    Added basic buttons

---
+++ packages/cui-react/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    plugins: [
+        react(),
+        dts({
+            include: 'src/**',
+        })
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'cui-react',
+            fileName: 'index',
+            formats: ['es', 'cjs'],
+        },
+        rollupOptions: {
+            external: [
+                'react',
+                "react/jsx-runtime",
+                'react-dom',
+            ],
+            output: {
+                globals: {
+                    'react': 'react',
+                    'react-dom': 'ReactDOM',
+                    'react/jsx-runtime': 'react/jsx-runtime',
+                },
+            },
+        }
+        // copyPublicDir: false,
+        // rollupOptions: {
+        //     external: ['react', 'react/jsx-runtime']
+        // }
+    }
+});
