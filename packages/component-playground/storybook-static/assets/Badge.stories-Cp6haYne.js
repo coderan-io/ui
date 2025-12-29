ce18c531d4b1c118b42622726cf287534f4c18c9
EranNL <dev@eranmachiels.nl>
2025-12-29 21:19:13 +0100

    Removed compiled code of storybook

--- packages/component-playground/storybook-static/assets/Badge.stories-Cp6haYne.js
+++
@@ -1,6 +0,0 @@
-import{a}from"./Card-BaWOGCjS.js";import{V as r}from"./Variant-BKuujzVJ.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-B-aL7s0A.js";import"./preload-helper-PPVm8Dsz.js";const t={RED:"red",PINK:"pink",PURPLE:"purple",INDIGO:"indigo",BLUE:"blue",TEAL:"teal",GREEN:"green",LIME:"lime",YELLOW:"yellow",ORANGE:"orange",BROWN:"brown",BLUE_GRAY:"blueGray",GRAY:"gray"},o={...t,PRIMARY:r.PRIMARY,DANGER:r.DANGER,SUCCESS:r.SUCCESS},p={title:"Example",component:a,args:{children:"Badge"},parameters:{layout:"centered"},argTypes:{color:{type:"select",options:Object.values(o)}}},e={args:{color:o.RED,children:"Red badge"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    color: BadgeColor.RED,
-    children: 'Red badge'
-  }
-}`,...e.parameters?.docs?.source}}};const g=["Badge"];export{e as Badge,g as __namedExportsOrder,p as default};
