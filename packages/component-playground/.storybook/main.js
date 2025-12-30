4c91d3a24e2872f1558cb7ae4f9213486b69c5dc
EranNL <dev@eranmachiels.nl>
2025-12-30 20:39:23 +0100

    Fixes

--- packages/component-playground/.storybook/main.js
+++ packages/component-playground/.storybook/main.js
@@ -3,7 +3,7 @@ const config = {
     stories: [
         '../stories/**/*.mdx',
         '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-        '../../components-react/src/**/*/*.stories.@(ts|tsx)'
+        '../../components-react/src/**/*/*.stories.@(ts|tsx)',
     ],
     addons: [
         '@storybook/addon-links',
